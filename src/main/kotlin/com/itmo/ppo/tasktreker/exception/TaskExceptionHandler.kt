2f9847e331f5cfde9a0af58bc7a72c2e9d85336c
ruaglsc <stanislav.ugolnikov@raiffeisen.ru>
2022-10-14 17:49:29 +0300

    перехват исключений

---
+++ src/main/kotlin/com/itmo/ppo/tasktreker/exception/TaskExceptionHandler.kt
@@ -0,0 +1,18 @@
+package com.itmo.ppo.tasktreker.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.persistence.EntityNotFoundException
+
+@ControllerAdvice
+class TaskExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException::class, IllegalArgumentException::class)
+    fun handle(e: Exception): ResponseEntity<*> {
+        return ResponseEntity.badRequest().body<Error>(Error(e.message))
+    }
+
+    private data class Error(
+        val error: String?
+    )
+}
